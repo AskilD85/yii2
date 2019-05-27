0c9e79ef2d378345bf463b0bc2eb4bda24cd65a2
askild85 <askildar@yandex.ru>
2019-05-27 10:42:00 +0500

    весь проект залил

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -57,11 +57,11 @@ AppAsset::register($this);
     }
     if (Yii::$app->user->isGuest) {
                 $menuItems[] = ['label' => 'Оставить заявку на подключение', 'url' => ['/request/create']];
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+                $menuItems[] = ['label' => 'Войти', 'url' => ['/site/login']];
             } else {
                 
                 $menuItems[] = [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+                    'label' => 'Выйти (' . Yii::$app->user->identity->username . ')',
                     'url' => ['/site/logout'],
                     'linkOptions' => ['data-method' => 'post']
                 ];
